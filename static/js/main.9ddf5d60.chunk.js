662dd5709ee2ba4abef839ed422e960847229b53
Malek Ammar <Malek.ammar@etudiant-enit.utm.tn>
2020-11-12 20:46:58 +0100

    Updates

---
+++ static/js/main.9ddf5d60.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpias-web-application"]=this["webpackJsonpias-web-application"]||[]).push([[0],{10:function(e,c,n){},11:function(e,c,n){"use strict";n.r(c);var t=n(0),i=n(1),a=n.n(i),s=n(3),p=n.n(s);n(9),n(10);var r=function(){return Object(t.jsx)("div",{className:"App",children:Object(t.jsx)("header",{className:"App-header",children:Object(t.jsx)("p",{children:"IAS Application"})})})};p.a.render(Object(t.jsx)(a.a.StrictMode,{children:Object(t.jsx)(r,{})}),document.getElementById("root"))},9:function(e,c,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.9ddf5d60.chunk.js.map
