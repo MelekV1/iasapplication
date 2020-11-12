37d38b57072cea2d0c9033b4feae2457eb7685a4
Malek Ammar <Malek.ammar@etudiant-enit.utm.tn>
2020-11-12 20:45:40 +0100

    deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>IAS Application</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
